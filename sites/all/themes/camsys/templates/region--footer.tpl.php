caed4b225ec714eb3743da2f822e05a2481de5e0
cqbent <cqbent@gmail.com>
2016-01-10 16:19:48 -0500

    update landing pages, styling, contact page, footer, etc.

--- sites/all/themes/camsys/templates/region--footer.tpl.php
+++ sites/all/themes/camsys/templates/region--footer.tpl.php
@@ -12,9 +12,9 @@
     <div class="inner">
       <?php print $content; ?>
       <div class="block social-links">
-        <a href="#"><i class="fa fa-facebook"></i></a>
-        <a href="#"><i class="fa fa-twitter"></i></a>
-        <a href="#"><i class="fa fa-linkedin"></i></a>
+        <a href="https://www.facebook.com/Camsys" target="_blank"><i class="fa fa-facebook"></i></a>
+        <a href="https://twitter.com/Camsys" target="_blank"><i class="fa fa-twitter"></i></a>
+        <a href="http://www.linkedin.com/company/cambridge-systematics/products" target="_blank"><i class="fa fa-linkedin"></i></a>
       </div>
       <div class="copyright">
         &copy; Copyright 2016 Cambridge Systematics. All rights reserved
