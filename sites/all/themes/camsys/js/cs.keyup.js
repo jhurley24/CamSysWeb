c4ddd267d85ef5b414e7c967637310a172e8a1c8
cqbent <cqbent@gmail.com>
2016-02-09 09:37:01 -0500

    update publications and autocomplete js

--- sites/all/themes/camsys/js/cs.keyup.js
+++ sites/all/themes/camsys/js/cs.keyup.js
@@ -12,52 +12,55 @@
      * related issue: https://www.drupal.org/node/309088
      *
      */
-    Drupal.jsAC.prototype.onkeyup = function (input, e) {
-        if (!e) {
-            e = window.event;
-        }
-        switch (e.keyCode) {
-            case 16: // Shift.
-            case 17: // Ctrl.
-            case 18: // Alt.
-            case 20: // Caps lock.
-            case 33: // Page up.
-            case 34: // Page down.
-            case 35: // End.
-            case 36: // Home.
-            case 37: // Left arrow.
-            case 38: // Up arrow.
-            case 39: // Right arrow.
-            case 40: // Down arrow.
-                return true;
+    if (typeof Drupal.jsAC != "undefined") {
+        Drupal.jsAC.prototype.onkeyup = function (input, e) {
+            if (!e) {
+                e = window.event;
+            }
+            switch (e.keyCode) {
+                case 16: // Shift.
+                case 17: // Ctrl.
+                case 18: // Alt.
+                case 20: // Caps lock.
+                case 33: // Page up.
+                case 34: // Page down.
+                case 35: // End.
+                case 36: // Home.
+                case 37: // Left arrow.
+                case 38: // Up arrow.
+                case 39: // Right arrow.
+                case 40: // Down arrow.
+                    return true;
 
-            case 9:  // Tab.
-            case 13: // Enter.
-            case 27: // Esc.
-                this.hidePopup(e.keyCode);
-                if (13 == e.keyCode && $(input).hasClass('auto_search_filter')) {
-                    input.form.submit();
-                }
-                return true;
-
-            default: // All other keys.
-                if (input.value.length > 0 && !input.readOnly) {
-                    this.populatePopup();
-                }
-                else {
+                case 9:  // Tab.
+                case 13: // Enter.
+                case 27: // Esc.
                     this.hidePopup(e.keyCode);
-                }
-                return true;
-        }
-    };
+                    if (13 == e.keyCode && $(input).hasClass('auto_search_filter')) {
+                        input.form.submit();
+                    }
+                    return true;
 
-    Drupal.jsAC.prototype.select = function (node) {
-        this.input.value = $(node).data('autocompleteValue');
-        $(this.input).trigger('autocompleteSelect', [node]);
-        if ($(this.input).hasClass('auto_search_filter')) {
-            this.input.form.submit();
-        }
-    };
+                default: // All other keys.
+                    if (input.value.length > 0 && !input.readOnly) {
+                        this.populatePopup();
+                    }
+                    else {
+                        this.hidePopup(e.keyCode);
+                    }
+                    return true;
+            }
+        };
+
+        Drupal.jsAC.prototype.select = function (node) {
+            this.input.value = $(node).data('autocompleteValue');
+            $(this.input).trigger('autocompleteSelect', [node]);
+            if ($(this.input).hasClass('auto_search_filter')) {
+                this.input.form.submit();
+            }
+        };
+    }
+
 
 
 
