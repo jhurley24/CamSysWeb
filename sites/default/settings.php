8aa1e7e9e47a16cb5b598e3775dbc6680c418247
Becky Soll <beckysoll@gmail.com>
2016-07-15 15:54:42 -0400

    I really think this should be working, let's see if it's a cache thing

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -20,9 +20,9 @@ if (isset($_SERVER['PANTHEON_ENVIRONMENT']) &&
 // 301 Redirect from /addyourword to surveygizmo url
 if (($_SERVER['REQUEST_URI'] == '/addyourword') &&
   (php_sapi_name() != "cli")) {
+  $newurl = 'http://www.google.com'];
   header('HTTP/1.0 301 Moved Permanently');
-  header('Location: http://www.surveygizmo.com/s3/2525676/Add-Your-Word/');
-  exit();
+  header("Location: $newurl");
 }
 
 
