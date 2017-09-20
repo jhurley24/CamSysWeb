52ccb73b85fa8323f7199432030b0fa55d2106a3
jhurley24 <jhurley@camsys.com>
2017-09-20 11:30:20 -0400

    Keyword Search Label

--- sites/all/themes/camsys/template.php
+++ sites/all/themes/camsys/template.php
@@ -218,6 +218,7 @@ function camsys_form_alter (&$form, &$form_state, $form_id) {
   }
     if ($form_id == 'global_filter_1') {
         $form['field_tags']['und']['#attributes']['class'] = array('auto_search_filter');
+        $form['#default_value'] = t('Keyword Search'); // Set a default value for the textfield
         $form['submit']['#value'] = 'Search';
         $form['submit']['#attributes']['style'] = array( 'display: none' );
         $form['submit']['#suffix'] = '<button type="submit" class="btn btn-primary"><i class="fa fa-search"></i></button>';
