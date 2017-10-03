3ea09eaa34d85bf475e2fa734b55652de33cc010
jhurley24 <jhurley@camsys.com>
2017-10-03 16:25:53 -0400

    Home Page Video Background - 2

--- sites/all/themes/camsys/templates/page--front.tpl.php
+++ sites/all/themes/camsys/templates/page--front.tpl.php
@@ -64,9 +64,9 @@
       <!-- home page content here -->
       <div class="region-content-top fullscreen-bg">
         <video loop muted autoplay poster="img/videoframe.jpg" class="fullscreen-bg__video">
-          <source src="video/big_buck_bunny.webm" type="video/webm">
-          <source src="video/big_buck_bunny.mp4" type="video/mp4">
-          <source src="video/big_buck_bunny.ogv" type="video/ogg">
+          <source src="../video/master_45th_1.webm" type="video/webm">
+          <source src="../video/master_45th_1.mp4" type="video/mp4">
+          <source src="../video/master_45th_1.ogv" type="video/ogg">
         </video>
         <?php print render($page['content']); ?>
       </div>
