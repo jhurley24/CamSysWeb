1c4bec9346dea72034ac514e40daa5ae23e20694
Dina Baker <camsyslicensing@camsys.com>
2018-01-09 19:46:50 +0000

    fixed linkedin address link in footer

--- sites/all/themes/camsys/templates/region--footer.tpl.php
+++ sites/all/themes/camsys/templates/region--footer.tpl.php
@@ -14,7 +14,7 @@
       <div class="block social-links">
         <a href="https://www.facebook.com/Camsys" target="_blank"><i class="fa fa-facebook"></i></a>
         <a href="https://twitter.com/Camsys" target="_blank"><i class="fa fa-twitter"></i></a>
-        <a href="http://www.linkedin.com/company/cambridge-systematics/products" target="_blank"><i class="fa fa-linkedin"></i></a>
+        <a href="http://www.linkedin.com/company/cambridge-systematics" target="_blank"><i class="fa fa-linkedin"></i></a>
       </div>
       <div class="copyright">
         &copy; Copyright 2018 Cambridge Systematics. All rights reserved 
